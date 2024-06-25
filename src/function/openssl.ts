3a4e8017ba97dcf650bad85621b4a7497ddbfd27
Clover <39840298+SmollClover@users.noreply.github.com>
2024-06-25 19:38:19 +0200

    Fix CN regex to be able to allow spaces near =

--- src/function/openssl.ts
+++ src/function/openssl.ts
@@ -2,10 +2,10 @@ export async function getCN(cert: string) {
     const proc = Bun.spawn(['openssl', 'x509', '-in', cert, '-noout', '-subject']);
 
     const output = await new Response(proc.stdout).text();
-    const CN = output.match(/CN=\S+/);
+    const CN = output.match(/CN ?= ?\S+/);
 
     if (!CN) throw new Error(`Could not find CN in certificate ${cert}`);
-    return CN[0].replace(/^CN=/m, '');
+    return CN[0].replace(/^CN ?= ?/m, '');
 }
 
 export async function genTLSA(usage: number, selector: number, matching_type: number) {
